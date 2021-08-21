8d97db3c25eb01cfac926a09993a160ea32f40b3
UnicusStella <unicusstella@gmail.com>
2021-08-21 18:05:08 +0900

    useState image setting

--- src/page/Gallery.js
+++ src/page/Gallery.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import Thumbnail from '../component/Thumbnail';
 import images from '../data/images'; // 제공되는 이미지를 이용하세요
 
 function Gallery() {
   // console.log(images) // npm start를 통해 앱을 열어, 개발자 콘솔을 통해 이미지 목록을 확인할 수 있습니다
 
+  const [image, setimage] = useState(images[0]);
   const handleClick = (image) => {
     // TIP: parameter로 이미지가 전달어야 하며, 이벤트 객체는 쓰지 않습니다
     // TODO
+    setimage(image);
   };
 
   return (
@@ -20,7 +23,7 @@ function Gallery() {
         {/* TODO: 아래 하드코딩된 내용 대신에, 목록 선택에 따른 이미지를 표시하세요 */}
         <h2>이미지 제목</h2>
         <img
-          src="현재이미지.jpeg"
+          src={image.src}
           id="current-image"
           alt="이미지가 없을 때 나오는 대체 텍스트"
         />
